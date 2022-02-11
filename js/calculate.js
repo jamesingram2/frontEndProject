f09e76be035aa4ff6a4775cfdb7c4767ad61fde7
James Ingram <jamesingram2@outlook.com>
2022-02-11 01:11:20 -0500

    finished crypto converter and updated css

--- js/calculate.js
+++ js/calculate.js
@@ -1,48 +1,25 @@
-// Template Code (will be updated with correct code later)
+let calculatorResultsDiv = document.getElementById("calculatorResults");
+document.getElementById("calculate").addEventListener('click', calcFunction);
+document.getElementById('reset').addEventListener('click', function() {
+   calculatorResultsDiv.innerHTML = "";
+   // window.location.reload();
+})
 
-// const select = document.querySelectorAll('.currency');
-// const number = document.getElementById("number");
-// const output = document.getElementById("output");
+function calcFunction() {
+    let currAmt = document.getElementById('currAmt').value;
+    let coin1 = document.getElementById('coin1').value;
+    let coin1Price = document.getElementById(coin1).innerHTML;
+    let coin2 = document.getElementById('coin2').value;
+    let coin2Price = document.getElementById(coin2).innerHTML;
 
-
-// fetch('https://api.frankfurter.app/currencies').then((data) => data.json())
-// .then((data) => {
-// display(data);
-// });
-
-
-// function display(data) {
-// const entries = Object.entries(data);
-// for (var i = 0; i < entries.length; i++) { select[0].innerHTML +=`<option value="${entries[i][0]}">${entries[i][0]} : ${entries[i][1]}</option>`;
-//     select[1].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]} : ${entries[i][1]}</option>`;
-//     }
-//     }
-
-
-
-//     function updatevalue() {
-//     let currency1 = select[0].value;
-//     let currency2 = select[1].value;
-
-//     let value = number.value;
-
-
-//     if (currency1 != currency2) {
-//     convert(currency1, currency2, value);
-//     } else {
-//     alert("Choose Diffrent Currency");
-//     }
-//     }
-
-
-//     function convert(currency1, currency2, value) {
-//     const host = "api.frankfurter.app";
-
-//     fetch(`https://${host}/latest?amount=${value}&from=${currency1}&to=${currency2}`)
-//     .then((val) => val.json())
-//     .then((val) => {
-//     console.log(Object.values(val.rates)[0]);
-//     output.value = Object.values(val.rates)[0];
-//     });
-
-//     }
+    if (currAmt <= 0) {
+        alert('Please enter a valid currency amount');
+    } else {
+        if (coin1 === coin2) {
+            alert('Please select two different currencies to compare');
+        } else {
+           const finalValue = ((+coin1Price / +coin2Price) * +currAmt).toFixed(4);
+            calculatorResultsDiv.innerHTML = `<h2>${currAmt} ${coin1.toUpperCase()} = ${finalValue} ${coin2.toUpperCase()}</h2>`;
+        }
+    }
+}
--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 async function fetchTable() {
+   const coin1El = document.getElementById("coin1");
+   const coin2El = document.getElementById("coin2");
    const coinTableDIV = document.getElementById('coinTable');
    const response = await fetch("https://api.coincap.io/v2/assets");
    const data = await response.json();
 
    const tableData = data.data;
-   let count = 0;
    tableData.forEach((coin) => {
+      let id = coin.id;
       let rank = coin.rank;
       let symbol = coin.symbol;
       let name = coin.name;
@@ -17,12 +19,15 @@ async function fetchTable() {
       <td>${name}</td>
       <td>${symbol}</td>
          <td>${rank}</td>
-         <td>${price}</td>
+         <td id="${symbol}">${price}</td>
          <td>${cap}</td>
          <td>${changePercent}</td>
       </tr>
       `;
       coinTableDIV.innerHTML += tempCoinHTNL;
+      coin1El.innerHTML += `<option value="${symbol}">${symbol}</option>`;
+      coin2El.innerHTML += `<option value="${symbol}">${symbol}</option>`;
    });
 }
 fetchTable();
+
