134159bc75c3b77c742bcd45188b3a811d2b1ed9
James Ingram <jamesingram2@outlook.com>
2022-02-25 14:51:24 -0500

    updated styling on index.html

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,10 @@ function cardHover(arr) {
    for (eventElement of cardArr) {
       let cardEl = document.getElementById(`${eventElement}`);
       cardEl.addEventListener('mouseover', function() {
-         cardEl.style.backgroundColor = "#0c0c0c";
-         cardEl.style.color = "#fff";
-         cardEl.style.border = "3px solid #ccc";
+         cardEl.style.backgroundColor = "darkgray";
       });
       cardEl.addEventListener('mouseout', function() {
-         cardEl.style.backgroundColor = "#fff";
-         cardEl.style.color = "#000"
-         cardEl.style.border = "";
+         cardEl.style = "";
       }) 
    }
 }
